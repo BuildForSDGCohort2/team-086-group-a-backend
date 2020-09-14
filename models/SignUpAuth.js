48eb3767f68b34800c66584108963a078a7e4585
hobbyluv <princewillchime43@gmail.com>
2020-09-14 15:18:15 +0100

    designing the signup schema

--- models/SignUpAuth.js
+++ models/SignUpAuth.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SignUpAuth = new Schema({
-  fullName: { type: String, maxlength: 30, required: true },
-  email: { type: String, maxlength: 100, required: true },
-  phoneNumber: { type: Number, maxlength: 30, required: true },
-  password: { type: String, required: true, maxlength: 15, minlength: 8 },
-  confirmPassword: {
-    type: String,
-    required: true,
-    maxlength: 15,
-    minlength: 8,
-  },
-  timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+const UserSubscription = Object({
+  category: { type: String },
+  subscribe: { type: Boolean },
 });
+
+const UserOrders = {
+  order: {
+    item: { type: String },
+    itemId: { type: String },
+    price: { type: Number },
+    vendorId: { type: String },
+  },
+};
+const SignUpAuth = new Schema(
+  {
+    fullName: { type: String, maxlength: 30, required: true },
+    email: { type: String, maxlength: 100, required: true },
+    phoneNumber: { type: Number, maxlength: 30, required: true },
+    password: { type: String, required: true, maxlength: 15, minlength: 8 },
+    confirmPassword: {
+      type: String,
+      required: true,
+      maxlength: 15,
+      minlength: 8,
+    },
+    userOrder: { UserOrders },
+    subcription: UserSubscription,
+  },
+  { timestamps: true }
+);
 
 const SignupUser = mongoose.model("SignUpAuth", SignUpAuth);
 module.exports = SignupUser;
