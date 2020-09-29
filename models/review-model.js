6b84bb9d3adfb488e3f65bd26df0cca55a8dc33d
hobbyluv <princewillchime43@gmail.com>
2020-09-29 01:36:16 +0100

    removed changes

--- models/review-model.js
+++ models/review-model.js
@@ -2,30 +2,33 @@ const { mongoose } = require("../configs/mongodb-config");
 
 const { Schema } = mongoose;
 
-const ReviewSchema = new mongoose.Schema({
-  customerId: {
-    type: Schema.Types.ObjectId,
-    ref: "Customers",
-    required: true,
+const ReviewSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Customers",
+      required: true,
+    },
+    vendorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Vendors",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      trim: " ",
+      minlength: 3,
+      maxlength: 255,
+    },
   },
-  vendorId: {
-    type: Schema.Types.ObjectId,
-    ref: "Vendors",
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 5,
-  },
-  comment: {
-    type: String,
-    trim: " ",
-    minlength: 3,
-    maxlength: 255,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 const ReviewModel = mongoose.model("Reviews", ReviewSchema);
 
 module.exports = ReviewModel;
--- routes/User.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const SignupUser = require("../controllers/UserSignup");
-
-//defining the signup route and passing of it malware
-router.post("user/signup", SignupUser.post_user_signUp);
-
-module.exports = router;
